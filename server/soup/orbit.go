7054db9fe1b0481917e364774df10a7e0deb29cc
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2025-02-25 19:31:30 +0100

    more values

--- server/soup/orbit.go
+++ server/soup/orbit.go
@@ -41,10 +41,28 @@ type Movement struct {
 	Z_avg float64
 	// Зенитное расстояние радианта
 	Z_fix float64
+	// Скорость V0 с учетом поправки за торможение
+	V0 float64
 	// Склонение радианта
 	Delta float64
+	// Прямое восхождение радианта. 0 < mov.Alpha < 2*pi
+	Alpha float64
+	// Эклиптическая широта радианта
+	Beta float64
+	// Эклиптическая долгота радианта
+	Lambda float64
+	// Долгота Солнца
+	Lambda_theta float64
+	// Гелиоцентрическая скорость
+	V_h float64
+	// Широта истинного радианта
+	Beta_deriv float64
 	// Часовой угол
 	T float64
+	// Долгота радианта относительно апекса
+	Diff_lambda float64
+	// Долгота истинного радианта
+	Lambda_deriv float64
 	// Звездное время в момент наблюдения
 	S float64
 	// Исправленные экваториальные координаты радианта
@@ -55,10 +73,42 @@ type Movement struct {
 	Delta_alpha float64
 	// Поправки за суточную аберрацию в экваториальных координатах
 	Delta_delta float64
+	// Элонгация
+	Psi_E float64
 	// Геоцентрическая скорость
 	V_g float64
+	// Орбитальная скорость Земли для данного дня
+	V_t float64
+	// Афелий – точка орбиты максимально удаленная от Солнца
+	Q float64
+	// Большая полуось
+	Axis float64
+	// Угол, образуемый радиус-вектором метеорного тела с вектором его скорости
+	Psi float64
+	// Элонгация радианта от Солнца
+	E_theta_deriv float64
 	// Внеатмосферная скорость
 	V_inf float64
+	// Наклонение орбиты частицы к плоскости эклиптики
+	Inc float64
+	// Элонгация истинного радианта от апекса
+	E_deriv float64
+	// Перигелийное расстояние
+	DistPer float64
+	// Долгота восходящего узла
+	Omega float64
+	// Аргумент перигелия
+	Wmega float64
+	// Истинная аномалия
+	Nu float64
+	// Эксцентриситет
+	Exc float64
+	// Часовой угол радианта
+	Angle float64
+	// Радиус вектор орбиты Земли
+	R float64
+	// Угол элонгации видимого радианта от апекса движения Земли
+	E_apex float64
 }
 
 type Input struct {
@@ -72,7 +122,8 @@ type Input struct {
 	Date time.Time
 }
 
-func NewMeteoroidMovement(inp Input) *Movement {
+func NewMeteoroidMovement(inp Input) (mv *Movement) {
+	mov := Movement{}
 	var temp float64
 	// step 1
 
@@ -81,303 +132,246 @@ func NewMeteoroidMovement(inp Input) *Movement {
 	// Главное значение азимута
 	A_gl := math.Atan((cos_phi1 - k*cos_phi2) / (k*sin_phi1 - sin_phi2))
 
-	// Азимут
-	var A float64
-
 	if inp.Tau1 <= 0 {
 		if inp.Tau2 < 0 {
 			if A_gl >= 0 {
-				A = A_gl
+				mov.A = A_gl
 			} else {
-				A = A_gl + math.Pi
+				mov.A = A_gl + math.Pi
 			}
 		} else {
 			if A_gl >= 0 {
-				A = A_gl
+				mov.A = A_gl
 			} else {
-				A = A_gl + 2*math.Pi
+				mov.A = A_gl + 2*math.Pi
 			}
 		}
 	} else { // inp.Tau1 > 0
 		if inp.Tau2 < 0 {
-			A = A_gl + math.Pi
+			mov.A = A_gl + math.Pi
 		} else {
 			if A_gl >= 0 {
-				A = A_gl + math.Pi
+				mov.A = A_gl + math.Pi
 			} else {
-				A = A_gl + 2*math.Pi
+				mov.A = A_gl + 2*math.Pi
 			}
 		}
 	}
-	sin_A, cos_A := math.Sincos(A)
+	sin_A, cos_A := math.Sincos(mov.A)
 
 	// step 2
 
 	temp = 2 * inp.V_avg * T
-	cos_A_minus_phi1 := math.Cos(A - phi1)
-	cos_A_minus_phi2 := math.Cos(A - phi2)
+	cos_A_minus_phi1 := math.Cos(mov.A - phi1)
+	cos_A_minus_phi2 := math.Cos(mov.A - phi2)
 	sin_z1 := (temp * inp.Tau1) / (l1 * cos_A_minus_phi1)
 	sin_z2 := (temp * inp.Tau2) / (l2 * cos_A_minus_phi2)
 
 	W1 := math.Abs(cos_A_minus_phi1)
 	W2 := math.Abs(cos_A_minus_phi2)
-	// Зенитный угол радианта
-	Z_avg := math.Asin((W1*sin_z1 + W2*sin_z2) / (W1 + W2))
+	mov.Z_avg = math.Asin((W1*sin_z1 + W2*sin_z2) / (W1 + W2))
 
 	// step 3
 
-	// Скорость V0 с учетом поправки за торможение
-	V0 := _1_0398*inp.V_avg + _0_65
+	mov.V0 = _1_0398*inp.V_avg + _0_65
 
 	// step 4
 
 	// Скорость
-	V_deriv := math.Sqrt(math.Pow(V0, 2) - _123_2)
-	delta_Z := 2 * math.Atan(math.Abs((V_deriv-V0))/(V_deriv+V0)*math.Tan(Z_avg/inp.V_avg))
-	// Зенитное расстояние радианта
-	Z_fix := Z_avg + delta_Z
-	sin_Z_fix, cos_Z_fix := math.Sincos(Z_fix)
+	V_deriv := math.Sqrt(math.Pow(mov.V0, 2) - _123_2)
+	delta_Z := 2 * math.Atan(math.Abs((V_deriv-mov.V0))/(V_deriv+mov.V0)*math.Tan(mov.Z_avg/inp.V_avg))
+	mov.Z_fix = mov.Z_avg + delta_Z
+	sin_Z_fix, cos_Z_fix := math.Sincos(mov.Z_fix)
 	sin_Z_fix_cos_A := sin_Z_fix * cos_A
 	sin_Z_fix_sin_A := sin_Z_fix * sin_A
 
 	// step 5
 
-	// Склонение радианта
-	delta := math.Asin(sin_phi*cos_Z_fix - cos_phi*sin_Z_fix_cos_A)
-	sin_delta, cos_delta := math.Sincos(delta)
+	mov.Delta = math.Asin(sin_phi*cos_Z_fix - cos_phi*sin_Z_fix_cos_A)
+	sin_delta, cos_delta := math.Sincos(mov.Delta)
 
 	// step 6
 
 	t_gl := math.Atan((sin_Z_fix_sin_A) / (cos_phi*cos_Z_fix + sin_phi*sin_Z_fix_cos_A))
-	// Часовой угол радианта
-	t := 0.
-	temp = sin_Z_fix_sin_A
+
 	if t_gl >= 0 {
-		if temp >= 0 {
-			t = t_gl
+		if sin_Z_fix_sin_A >= 0 {
+			mov.Angle = t_gl
 		} else {
-			t = t_gl + math.Pi
+			mov.Angle = t_gl + math.Pi
 		}
 	} else { // t_gl < 0
-		if temp >= 0 {
-			t = t_gl + math.Pi
+		if sin_Z_fix_sin_A >= 0 {
+			mov.Angle = t_gl + math.Pi
 		} else {
-			t = t_gl + 2*math.Pi
+			mov.Angle = t_gl + 2*math.Pi
 		}
 	}
-	sin_t, cos_t := math.Sincos(t)
+	sin_t, cos_t := math.Sincos(mov.Angle)
 
 	// step 7
 
-	// Звездное время в момент наблюдения
-	S := StellarTime(c2, inp.Date.YearDay()-1, inp.Date.Hour(), inp.Date.Minute())
+	mov.S = StellarTime(c2, inp.Date.YearDay()-1, inp.Date.Hour(), inp.Date.Minute())
 
 	// step 8
 
-	// Прямое восхождение радианта
-	alpha := S - t // 0 < alpha < 2 pi (6.28)
+	mov.Alpha = mov.S - mov.Angle
 
 	// step 9
 
-	// Поправки за суточную аберрацию в экваториальных координатах
-	delta_alpha := -((_0_4703 * cos_t * cos_phi) / (V_deriv * cos_delta))
-	// Поправки за суточную аберрацию в экваториальных координатах
-	delta_delta := -((_0_4703 * sin_t * sin_delta * cos_phi) / (V_deriv))
+	mov.Delta_alpha = -((_0_4703 * cos_t * cos_phi) / (V_deriv * cos_delta))
+	mov.Delta_delta = -((_0_4703 * sin_t * sin_delta * cos_phi) / (V_deriv))
 
 	// step 10
 
-	// Исправленные экваториальные координаты радианта
-	alpha_fix := alpha + delta_alpha // while alpha_fix > 0
-	sin_alpha_fix, cos_alpha_fix := math.Sincos(alpha_fix)
-	// Исправленные экваториальные координаты радианта
-	delta_fix := delta + delta_delta
-	sin_delta_fix, cos_delta_fix := math.Sincos(delta_fix)
+	mov.Alpha_fix = mov.Alpha + mov.Delta_alpha // while alpha_fix > 0
+	sin_alpha_fix, cos_alpha_fix := math.Sincos(mov.Alpha_fix)
+	mov.Delta_fix = mov.Delta + mov.Delta_delta
+	sin_delta_fix, cos_delta_fix := math.Sincos(mov.Delta_fix)
 
 	// step 11
 
 	psi_E_gl := math.Acos(-sin_t * cos_delta_fix)
-	// Элонгация
-	psi_E := psi_E_gl
+	mov.Psi_E = psi_E_gl
 	if psi_E_gl < 0 {
-		psi_E += math.Pi
+		mov.Psi_E += math.Pi
 	}
 
 	// step 12
 
-	delta_s := math.Sqrt(math.Pow(delta_alpha*cos_delta_fix, 2) + math.Pow(delta_delta, 2))
-	// Геоцентрическая скорость
-	V_g := V_deriv * (math.Sin(psi_E-delta_s) / math.Sin(psi_E))
-	pow_2_V_g := math.Pow(V_g, 2)
+	delta_s := math.Sqrt(math.Pow(mov.Delta_alpha*cos_delta_fix, 2) + math.Pow(mov.Delta_delta, 2))
+	mov.V_g = V_deriv * (math.Sin(mov.Psi_E-delta_s) / math.Sin(mov.Psi_E))
+	pow_2_V_g := math.Pow(mov.V_g, 2)
 
 	// step 13
 
-	// Внеатмосферная скорость
-	V_inf := math.Sqrt(pow_2_V_g + _123_2)
+	mov.V_inf = math.Sqrt(pow_2_V_g + _123_2)
 
 	// step 14
 
-	// Эклиптическая широта радианта
-	beta := math.Asin(-sin_e*sin_alpha_fix*cos_delta_fix + cos_e + sin_delta_fix)
-	sin_beta, cos_beta := math.Sincos(beta)
+	mov.Beta = math.Asin(-sin_e*sin_alpha_fix*cos_delta_fix + cos_e + sin_delta_fix)
+	sin_beta, cos_beta := math.Sincos(mov.Beta)
 
 	// step 15
 
 	cos_lambda := ((cos_delta_fix * cos_alpha_fix) / cos_beta)
 	sin_lambda := (1 / cos_beta) * (cos_delta_fix*sin_alpha_fix*cos_e + sin_delta_fix*sin_e)
-	// Эклиптическая долгота радианта
-	lambda := math.Atan2(sin_lambda, cos_lambda)
+	mov.Lambda = math.Atan2(sin_lambda, cos_lambda)
 
 	// step 16
 
-	// Долгота Солнца
-	lambda_theta := SolarLongitude(c3, inp.Date.YearDay()-1, inp.Date.Hour(), inp.Date.Minute())
+	mov.Lambda_theta = SolarLongitude(c3, inp.Date.YearDay()-1, inp.Date.Hour(), inp.Date.Minute())
 
 	// step 17
 
-	delta_theta := _0_01672 * math.Sin(lambda_theta-Pi0)
-	lambda_apex := lambda_theta + delta_theta - (math.Pi / 2)
-	// Долгота радианта относительно апекса
-	diff_lambda := lambda - lambda_apex
-	_ = diff_lambda
+	delta_theta := _0_01672 * math.Sin(mov.Lambda_theta-Pi0)
+	lambda_apex := mov.Lambda_theta + delta_theta - (math.Pi / 2)
+	mov.Diff_lambda = mov.Lambda - lambda_apex
 
 	// step 18
 
-	// Угол элонгации видимого радианта от апекса движения Земли
-	E_apex := math.Acos(cos_beta * math.Cos(lambda-lambda_apex))
+	mov.E_apex = math.Acos(cos_beta * math.Cos(mov.Lambda-lambda_apex))
 
 	// step 19
 
-	// Радиус вектор орбиты Земли
-	R := (1 - math.Pow(e0, 2)) / (1 - e0*math.Cos(lambda_theta-Pi0))
+	mov.R = (1 - math.Pow(e0, 2)) / (1 - e0*math.Cos(mov.Lambda_theta-Pi0))
 
 	// step 20
 
-	// Орбитальная скорость Земли для данного дня
-	V_t := 29.76 * math.Sqrt((2/R)-1)
+	mov.V_t = 29.76 * math.Sqrt((2/mov.R)-1)
 
 	// step 21
 
-	temp = lambda_theta + delta_theta - lambda
+	temp = mov.Lambda_theta + delta_theta - mov.Lambda
 	sin_temp, cos_temp := math.Sincos(temp)
-	temp_deriv_gl := math.Atan2(sin_temp-(V_t/(V_g*cos_beta)), cos_temp)
+	temp_deriv_gl := math.Atan2(sin_temp-(mov.V_t/(mov.V_g*cos_beta)), cos_temp)
 	temp_deriv := temp_deriv_gl
 	if cos_temp < 0 {
 		temp_deriv += math.Pi
 	}
-	// Долгота истинного радианта
-	lambda_deriv := lambda_theta + delta_theta - temp_deriv // 0 <= lambda_deriv <= 2*pi
-	sin_lambda_diff, cos_lambda_diff := math.Sincos(lambda_theta - lambda_deriv)
+	mov.Lambda_deriv = mov.Lambda_theta + delta_theta - temp_deriv // 0 <= lambda_deriv <= 2*pi
+	sin_lambda_diff, cos_lambda_diff := math.Sincos(mov.Lambda_theta - mov.Lambda_deriv)
 
 	// step 22
 
-	// Гелиоцентрическая скорость
-	V_h := math.Sqrt(pow_2_V_g + math.Pow(V_t, 2) - 2*V_g*V_t*math.Cos(E_apex))
+	mov.V_h = math.Sqrt(pow_2_V_g + math.Pow(mov.V_t, 2) - 2*mov.V_g*mov.V_t*math.Cos(mov.E_apex))
 
 	// step 23
 
-	// Широта истинного радианта
-	beta_deriv := math.Asin((V_g / V_h) * sin_beta)
-	cos_beta_deriv := math.Cos(beta_deriv)
+	mov.Beta_deriv = math.Asin((mov.V_g / mov.V_h) * sin_beta)
+	cos_beta_deriv := math.Cos(mov.Beta_deriv)
 
 	// step 24
 
-	// Элонгация истинного радианта от апекса
-	E_deriv := math.Acos(cos_beta_deriv * math.Cos(lambda_deriv-lambda_apex))
-	_ = E_deriv
+	mov.E_deriv = math.Acos(cos_beta_deriv * math.Cos(mov.Lambda_deriv-lambda_apex))
 
 	// step 25
 
-	i_gl := math.Atan(-(math.Abs(math.Tan(beta_deriv)) / sin_lambda_diff))
-	// Наклонение орбиты частицы к плоскости эклиптики
-	i := i_gl
+	i_gl := math.Atan(-(math.Abs(math.Tan(mov.Beta_deriv)) / sin_lambda_diff))
+	mov.Inc = i_gl
 	if i_gl > 0 {
-		i += math.Pi
+		mov.Inc += math.Pi
 	}
 
 	// step 26
 
-	// Афелий – точка орбиты максимально удаленная от Солнца
-	Q := math.Pow(V_h/V_t, 2)
+	mov.Q = math.Pow(mov.V_h/mov.V_t, 2)
 
 	// step 27
 
-	// Большая полуось
-	a := 1 / ((2 - Q) / R)
+	mov.Axis = 1 / ((2 - mov.Q) / mov.R)
 
 	// step 28
 
-	// Угол, образуемый радиус-вектором метеорного тела с вектором его скорости
-	psi := math.Acos(-cos_beta_deriv * cos_lambda_diff)
-	// Элонгация радианта от Солнца
-	E_theta_deriv := math.Pi - psi
+	mov.Psi = math.Acos(-cos_beta_deriv * cos_lambda_diff)
+	mov.E_theta_deriv = math.Pi - mov.Psi
 
 	// step 29
 
 	// Параметр орбиты
-	p := math.Pow(R, 2) * Q * math.Pow(math.Sin(E_theta_deriv), 2)
-	b := math.Sqrt(p * math.Abs(a))
+	p := math.Pow(mov.R, 2) * mov.Q * math.Pow(math.Sin(mov.E_theta_deriv), 2)
+	b := math.Sqrt(p * math.Abs(mov.Axis))
 
 	// step 30
 
 	var c float64
-	if a > 0 {
-		c = math.Sqrt(math.Pow(a, 2) - math.Pow(b, 2))
+	if mov.Axis > 0 {
+		c = math.Sqrt(math.Pow(mov.Axis, 2) - math.Pow(b, 2))
 	} else {
-		c = math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+		c = math.Sqrt(math.Pow(mov.Axis, 2) + math.Pow(b, 2))
 	}
-	// Эксцентриситет
-	e = math.Abs(c / a)
+	mov.Exc = math.Abs(c / mov.Axis)
 
 	// step 31
 
-	// Перигелийное расстояние
-	q := a - c
-	if a < 0 {
-		q = c - math.Abs(a)
+	mov.DistPer = mov.Axis - c
+	if mov.Axis < 0 {
+		mov.DistPer = c - math.Abs(mov.Axis)
 	}
-	_ = q
 
 	// step 32
 
-	// Долгота восходящего узла
-	omega := lambda_theta
-	if beta_deriv < 0 {
-		omega += math.Pi
+	mov.Omega = mov.Lambda_theta
+	if mov.Beta_deriv < 0 {
+		mov.Omega += math.Pi
 	}
-	_ = omega
 
 	// step 33
 
-	cos_v := (p - R) / (R * e)
-	sin_v := p / (R * e) * math.Cos(i) * Ctg(lambda_deriv-lambda_theta)
-	// Истинная аномалия
-	v := math.Atan2(sin_v, cos_v)
+	cos_v := (p - mov.R) / (mov.R * e)
+	sin_v := p / (mov.R * e) * math.Cos(mov.Inc) * Ctg(mov.Lambda_deriv-mov.Lambda_theta)
+	mov.Nu = math.Atan2(sin_v, cos_v)
 
 	// step 34
 
-	// Аргумент перигелия
-	var wmega float64
-	if beta_deriv > 0 {
-		wmega = math.Pi - v
+	if mov.Beta_deriv > 0 {
+		mov.Wmega = math.Pi - mov.Nu
 	} else {
-		wmega = -v
+		mov.Wmega = -mov.Nu
 	}
-	_ = wmega
 
-	return &Movement{
-		A:           A,
-		Z_avg:       Z_avg,
-		Z_fix:       Z_fix,
-		Delta:       delta,
-		T:           t,
-		Alpha_fix:   alpha_fix,
-		Delta_fix:   delta_fix,
-		Delta_alpha: delta_alpha,
-		Delta_delta: delta_delta,
-		S:           S,
-		V_g:         V_g,
-		V_inf:       V_inf,
-	}
+	mv = &mov
+	return
 }
 
 // Format: 2006-01-02T03:04
