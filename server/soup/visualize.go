3f5759bde5d248acd24ab2273f87ad79cc060e05
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2025-05-02 20:32:59 +0200

    move visualization to another file

---
+++ server/soup/visualize.go
@@ -0,0 +1,70 @@
+package soup
+
+import (
+	"fmt"
+	"io"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+)
+
+type AngleTicker struct {
+	Step float64
+}
+
+func (t AngleTicker) Ticks(min, max float64) []plot.Tick {
+	var ticks []plot.Tick
+	for val := min; val <= max; val += t.Step {
+		ticks = append(ticks, plot.Tick{Value: val, Label: fmt.Sprintf("%.1f°", val)})
+	}
+	return ticks
+}
+
+func Visualize(tests []MovementTest) (io.WriterTo, error) {
+	p := plot.New()
+	p.X.Label.Text = "Right Ascension"
+	p.Y.Label.Text = "Declination"
+	p.X.Tick.Length = 5
+	p.Y.Tick.Length = 5
+	p.X.Tick.Marker = AngleTicker{Step: 45}
+	p.Y.Tick.Marker = AngleTicker{Step: 10}
+	p.X.Min = 0
+	p.X.Max = 360
+	p.Y.Min = -90
+	p.Y.Max = 90
+
+	pointsActual := plotter.XYs{}
+	// pointsExpected := plotter.XYs{}
+	for _, m := range tests {
+		if m.Actual.Fail != nil {
+			continue
+		}
+		pointsActual = append(pointsActual, plotter.XY{X: m.Actual.Alpha, Y: m.Actual.Delta})
+		// pointsExpected = append(pointsActual, plotter.XY{X: m.Expected.Alpha, Y: m.Expected.Delta})
+	}
+	scatter, err := plotter.NewScatter(pointsActual)
+	if err != nil {
+		return nil, err
+	}
+	scatter.Shape = draw.CircleGlyph{}
+	scatter.GlyphStyle.Radius = vg.Points(.4)
+	p.Add(scatter)
+	// scatter2, err := plotter.NewScatter(pointsExpected)
+	// if err != nil {
+	// 	return err
+	// }
+	// scatter2.Shape = draw.CircleGlyph{}
+	// scatter2.GlyphStyle.Radius = vg.Points(.2)
+	// scatter2.GlyphStyle.Color = color.RGBA{255, 0, 0, 255}
+	// p.Add(scatter2)
+
+	// Add a grid
+	grid := plotter.NewGrid()
+	p.Add(grid)
+
+	scale := 1
+	w, h := 4*80*scale, 3*80*scale
+	return p.WriterTo(vg.Length(w*scale), vg.Length(h*scale), "png")
+}
