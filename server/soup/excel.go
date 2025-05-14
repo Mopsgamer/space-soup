78eafb9412be8b1cbc4c960b55f27e12345b92fa
Mopsgamer <kisillev020304@gmail.com>
2025-05-14 17:27:57 +0200

    format the table somehow

--- server/soup/excel.go
+++ server/soup/excel.go
@@ -10,36 +10,56 @@ func NewFileExcelBytes(movementList []MovementTest) ([]byte, error) {
 	file := excelize.NewFile()
 	sheetName := file.GetSheetName(0)
 
+	bordered, _ := file.NewStyle(&excelize.Style{Border: []excelize.Border{
+		{Type: "left", Style: 1, Color: "000000"},
+		{Type: "top", Style: 1, Color: "000000"},
+		{Type: "right", Style: 1, Color: "000000"},
+		{Type: "bottom", Style: 1, Color: "000000"},
+	}})
+	// Add header row
+	headers := []string{"V_avg", "Tau1", "Tau2", "Lambda_apex", "A", "Z_avg", "Delta", "Alpha", "Beta", "Lambda", "Lambda_deriv", "Beta_deriv", "Inc", "Wmega", "Omega", "V_g", "V_h", "Axis", "Exc", "Nu"}
+	for colIndex, header := range headers {
+		cell, _ := excelize.CoordinatesToCellName(colIndex+1, 1)
+		file.SetCellValue(sheetName, cell, header)
+		file.SetCellStyle(sheetName, cell, cell, bordered)
+	}
+
 	colIndex := 0
 	nextColCell := func(row int) string {
 		colIndex += 1
 		cell, _ := excelize.CoordinatesToCellName(colIndex, row)
 		return cell
 	}
+
 	for rowIndex, movement := range movementList {
-		row := rowIndex + 1
+		row := rowIndex + 2 // Start from row 2 as row 1 is the header
 		colIndex = 0
+		setCell := func(v float64) {
+			coord := nextColCell(row)
+			file.SetCellFloat(sheetName, coord, v, 2, 32)
+			file.SetCellStyle(sheetName, coord, coord, bordered)
+		}
 		// V_avg	Tau1	Tau2	Lambda_apex	A	Z_avg	Delta	Alpha	Beta	Lambda	Lambda_deriv	Beta_deriv	Inc	Wmega	Omega	V_g	V_h	Axis	Exc	Nu
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Input.V_avg, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Input.Tau1, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Input.Tau2, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Lambda_apex, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.A, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Z_avg, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Delta, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Alpha, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Beta, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Lambda, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Lambda_deriv, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Beta_deriv, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Inc, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Wmega, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Omega, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.V_g, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.V_h, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Axis, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Exc, 2, 32)
-		file.SetCellFloat(sheetName, nextColCell(row), movement.Actual.Nu, 2, 32)
+		setCell(movement.Input.V_avg)
+		setCell(movement.Input.Tau1)
+		setCell(movement.Input.Tau2)
+		setCell(movement.Actual.Lambda_apex)
+		setCell(movement.Actual.A)
+		setCell(movement.Actual.Z_avg)
+		setCell(movement.Actual.Delta)
+		setCell(movement.Actual.Alpha)
+		setCell(movement.Actual.Beta)
+		setCell(movement.Actual.Lambda)
+		setCell(movement.Actual.Lambda_deriv)
+		setCell(movement.Actual.Beta_deriv)
+		setCell(movement.Actual.Inc)
+		setCell(movement.Actual.Wmega)
+		setCell(movement.Actual.Omega)
+		setCell(movement.Actual.V_g)
+		setCell(movement.Actual.V_h)
+		setCell(movement.Actual.Axis)
+		setCell(movement.Actual.Exc)
+		setCell(movement.Actual.Nu)
 	}
 
 	var buf bytes.Buffer
