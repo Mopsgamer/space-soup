2a442d350bbc8940eb9d7ebf3022c1692e83c87b
Mopsgamer <kisillev020304@gmail.com>
2025-05-15 15:44:17 +0200

    styling works

--- server/soup/excel.go
+++ server/soup/excel.go
@@ -2,6 +2,8 @@ package soup
 
 import (
 	"bytes"
+	"fmt"
+	"strings"
 
 	"github.com/xuri/excelize/v2"
 )
@@ -10,21 +12,76 @@ func NewFileExcelBytes(movementList []MovementTest) ([]byte, error) {
 	file := excelize.NewFile()
 	sheetName := file.GetSheetName(0)
 
-	bordered, _ := file.NewStyle(&excelize.Style{
-		Font: &excelize.Font{Family: "Times New Roman"},
+	normalCfg := &excelize.Style{
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+			Vertical:   "center",
+			WrapText:   true,
+		},
+		Font: &excelize.Font{Family: "Times New Roman", Color: "000000", Size: 14},
 		Border: []excelize.Border{
 			{Type: "left", Style: 1, Color: "000000"},
 			{Type: "top", Style: 1, Color: "000000"},
 			{Type: "right", Style: 1, Color: "000000"},
 			{Type: "bottom", Style: 1, Color: "000000"},
 		},
-	})
+	}
+	normal, _ := file.NewStyle(normalCfg)
 
-	headers := []string{"#", "V_avg", "Tau1", "Tau2", "Lambda_apex", "A", "Z_avg", "Delta", "Alpha", "Beta", "Lambda", "Lambda_deriv", "Beta_deriv", "Inc", "Wmega", "Omega", "V_g", "V_h", "Axis", "Exc", "Nu"}
-	for colIndex, header := range headers {
-		cell, _ := excelize.CoordinatesToCellName(colIndex+1, 1)
-		file.SetCellValue(sheetName, cell, header)
-		file.SetCellStyle(sheetName, cell, cell, bordered)
+	headerCfg := &excelize.Style{
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+			Vertical:   "center",
+			WrapText:   true,
+		},
+		Font: &excelize.Font{
+			Family: "Times New Roman",
+			Color:  "000000",
+			Size:   14,
+			Bold:   true,
+		},
+		Border: []excelize.Border{
+			{Type: "left", Style: 1, Color: "000000"},
+			{Type: "top", Style: 1, Color: "000000"},
+			{Type: "right", Style: 1, Color: "000000"},
+			{Type: "bottom", Style: 1, Color: "000000"},
+		},
+	}
+	header, _ := file.NewStyle(headerCfg)
+
+	headers := []string{
+		"№",
+		"№ метеороїда",
+		"Середня швидкість (V_avg)",
+		"Часова затримка 1 (Tau1)",
+		"Часова затримка 2 (Tau2)",
+		"Довгота апексу (Lambda_apex)",
+		"Азимут (A)",
+		"Зенітний кут радіанта (Z_avg)",
+		"Схиляння радіанта (Delta)",
+		"Пряме сходження радіанта (Alpha)",
+		"Екліптична широта радіанта (Beta)",
+		"Екліптична довгота радіанта (Lambda)",
+		"Довгота істинного радіанта (Lambda_deriv)",
+		"Широта істинного радіанта (Beta_deriv)",
+		"Нахил орбіти (Inc)",
+		"Довгота висхідного вузла (Wmega)",
+		"Аргумент перигелію (Omega)",
+		"Геоцентрична швидкість (V_g)",
+		"Геліоцентрична швидкість (V_h)",
+		"Велика піввісь (Axis)",
+		"Ексцентриситет (Exc)",
+		"Істинна аномалія (Nu)",
+	}
+	for colIndex, label := range headers {
+		colIndex += 1
+		cell, _ := excelize.CoordinatesToCellName(colIndex, 1)
+		file.SetCellValue(sheetName, cell, label)
+		file.SetCellStyle(sheetName, cell, cell, header)
+
+		cell2, _ := excelize.CoordinatesToCellName(colIndex, 2)
+		file.SetCellValue(sheetName, cell2, colIndex)
+		file.SetCellStyle(sheetName, cell2, cell2, header)
 	}
 
 	colIndex := 0
@@ -35,18 +92,21 @@ func NewFileExcelBytes(movementList []MovementTest) ([]byte, error) {
 	}
 
 	for rowIndex, movement := range movementList {
-		row := rowIndex + 2 // Start from row 2 as row 1 is the header
+		rowIndex += 1
+		row := rowIndex + 2
 		colIndex = 0
 		setCellInt := func(v int) {
 			coord := nextColCell(row)
 			file.SetCellInt(sheetName, coord, v)
-			file.SetCellStyle(sheetName, coord, coord, bordered)
+			file.SetCellStyle(sheetName, coord, coord, normal)
 		}
 		setCellFloat := func(v float64) {
 			coord := nextColCell(row)
-			file.SetCellFloat(sheetName, coord, v, 2, 32)
-			file.SetCellStyle(sheetName, coord, coord, bordered)
+			str := strings.Replace(fmt.Sprintf("%.2f", v), ".", ",", 1)
+			file.SetCellValue(sheetName, coord, str)
+			file.SetCellStyle(sheetName, coord, coord, normal)
 		}
+		setCellInt(rowIndex + 1)
 		setCellInt(movement.Input.Id)
 		setCellFloat(movement.Input.V_avg)
 		setCellFloat(movement.Input.Tau1)
